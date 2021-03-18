919f07f3c32033edcfefb8ca93c76490e7f1cfcb
MichaelCook-v2 <73264730+MichaelCook-v2@users.noreply.github.com>
2021-03-17 21:53:56 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+userName = '';
+
+userName ? console.log('Hello!') : console.log(`Hello ${userName}!`)
+
+const userQuestion = '';
+console.log(`The user asked: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random()* 8);
+
+let eightBall = '';
+switch (randomNumber){
+  case 0:
+    eightBall ='It is certain';
+    break;
+  case 1:
+    eightBall ='It is decidedly so';
+    break;
+  case 2:
+    eightBall ='Reply hazy try again';
+    break;
+  case 3:
+    eightBall ='Cannot predict now';
+    break;
+  case 4:
+     eightBall ='Do not count on it';
+    break;
+  case 5:
+     eightBall ='My sources say no';
+    break;
+  case 6:
+     eightBall ='Outlook not so good';
+    break;
+  case 7:
+     eightBall ='Signs point to yes';
+    default:
+     eightBall ='invalid number';
+    break;
+}
+console.log(`The eight ball answered: ${eightBall}`);
